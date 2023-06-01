707e95af2ca0646f3520149e641cb827b935c58f
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-06-01 17:44:03 +0600

    added Header, HTTPCode

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,22 +1,13 @@
-import {
-  Controller,
-  Post,
-  Req,
-  HttpCode,
-  HttpStatus,
-  Res,
-  // Header,
-} from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Controller, Post, Req, Header, HttpCode } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('/users')
 export class UsersController {
   @Post('/profile')
-  // @Header('Cache-Control', 'none')
-  // @Header('X-name', 'jadid')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  getProfile(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
-    res.status(200);
+  @Header('Cache-Control', 'none')
+  @Header('X-name', 'jadid')
+  @HttpCode(200)
+  getProfile(@Req() req: Request) {
     return {
       hello: 'Jadid',
     };
