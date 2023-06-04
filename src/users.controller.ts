f32090b6dc4a453efbf171f44eef9a8712e87ca1
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2023-06-04 16:39:29 +0600

    worked with query parameters

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,16 +1,15 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 
-interface videoParams {
-  id: number;
+interface QueryParams {
+  age: number;
   name: string;
 }
 
 @Controller('/users')
 export class UsersController {
-  @Get('/videos/:id/:name')
-  getVideos(@Param() params: videoParams) {
-    console.log(params.id);
-    console.log(params.name);
+  @Get('/videos')
+  getVideos(@Query() query: QueryParams) {
+    console.log(query.age, query.name);
     return 'Success!';
   }
 }
